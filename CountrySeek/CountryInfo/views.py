07a044d276d45cc14670e560e712b08a140e743f
author <qwefghnz@gmail.com>
2023-12-28 14:25:47 +0600

    corrected volumes in docker-compose.yml so that updates will be visible

--- CountrySeek/CountryInfo/views.py
+++ CountrySeek/CountryInfo/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def mainPage(request):
     return render(request, 'CountryInfo/index.html')
+
+def search(request):
+    if request.method == 'POST':
+        q_text = request.POST.get("q_name")
+        return HttpResponse(f'<h1>You looked for {q_text}</h1>')
